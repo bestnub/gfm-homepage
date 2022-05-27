64590d03825599c8970468596c4fad8e5374db72
BestNub <jonas.hoefler@gmail.com>
2022-05-27 19:12:08 +0200

    Added compression

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,34 @@
 const {
   defineConfig
 } = require('@vue/cli-service')
+const zlib = require("zlib");
 module.exports = defineConfig({
   transpileDependencies: true,
   css: {
     loaderOptions: {
       scss: {
         additionalData: `@import "@/assets/_theme.scss";`
+      }
+    }
+  },
+  pluginOptions: {
+    compression: {
+      brotli: {
+        filename: '[file].br[query]',
+        algorithm: 'brotliCompress',
+        include: /\.(js|css|html|svg|json)(\?.*)?$/i,
+        compressionOptions: {
+          params: {
+            [zlib.constants.BROTLI_PARAM_QUALITY]: 11,
+          },
+        },
+        minRatio: 0.8,
+      },
+      gzip: {
+        filename: '[file].gz[query]',
+        algorithm: 'gzip',
+        include: /\.(js|css|html|svg|json)(\?.*)?$/i,
+        minRatio: 0.8,
       }
     }
   }
