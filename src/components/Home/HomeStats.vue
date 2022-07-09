d18b419fa72a1f3f177f6f43d27c35e46b242bfa
bestnub <63472070+bestnub@users.noreply.github.com>
2022-07-09 21:34:45 +0200

    Fix HomepageStats

--- src/components/Home/HomeStats.vue
+++ src/components/Home/HomeStats.vue
@@ -5,23 +5,16 @@
         <div class="surface-card shadow-2 p-3 border-round">
           <div class="flex justify-content-between mb-3">
             <div>
-              <span class="block text-500 font-medium mb-3"
-                >TeamSpeak Nutzer</span
-              >
-              <div v-if="stats" class="text-900 font-medium text-xl">
-                {{ stats.tsUserCount }}
-              </div>
+              <span class="block text-500 font-medium mb-3">TeamSpeak Nutzer</span>
+              <div v-if="stats" class="text-900 font-medium text-xl text-left">{{ stats.tsUserCount }}</div>
             </div>
-            <div
-              class="
+            <div class="
                 flex
                 align-items-center
                 justify-content-center
                 bg-blue-100
                 border-round
-              "
-              style="width: 2.5rem; height: 2.5rem"
-            >
+                " style="width: 2.5rem; height: 2.5rem">
               <i class="pi pi-users text-blue-500 text-xl"></i>
             </div>
           </div>
@@ -36,18 +29,15 @@
           <div class="flex justify-content-between mb-3">
             <div>
               <span class="block text-500 font-medium mb-3">Spiele</span>
-              <div class="text-900 font-medium text-xl">5+</div>
+              <div class="text-900 font-medium text-xl text-left">5+</div>
             </div>
-            <div
-              class="
+            <div class="
                 flex
                 align-items-center
                 justify-content-center
                 bg-orange-100
                 border-round
-              "
-              style="width: 2.5rem; height: 2.5rem"
-            >
+              " style="width: 2.5rem; height: 2.5rem">
               <i class="pi pi-desktop text-orange-500 text-xl"></i>
             </div>
           </div>
@@ -59,20 +49,15 @@
           <div class="flex justify-content-between mb-3">
             <div>
               <span class="block text-500 font-medium mb-3">GFM Nutzer</span>
-              <div v-if="stats" class="text-900 font-medium text-xl">
-                {{ stats.gfmUserCount }}
-              </div>
+              <div v-if="stats" class="text-900 font-medium text-xl text-left">{{ stats.gfmUserCount }}</div>
             </div>
-            <div
-              class="
+            <div class="
                 flex
                 align-items-center
                 justify-content-center
                 bg-cyan-100
                 border-round
-              "
-              style="width: 2.5rem; height: 2.5rem"
-            >
+              " style="width: 2.5rem; height: 2.5rem">
               <i class="pi pi-user text-cyan-500 text-xl"></i>
             </div>
           </div>
