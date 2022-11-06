cef04dc8dab31dbb900750f96b957daa387dca44
bestnub <63472070+bestnub@users.noreply.github.com>
2022-11-06 16:06:14 +0100

    Added Loading animation to status

--- src/components/Games/GameCard.vue
+++ src/components/Games/GameCard.vue
@@ -3,18 +3,15 @@
     <card class="flex flex-column" style="width: 100%; height: 100%">
       <template #header>
         <div class="flex justify-content-center">
-          <img
-            v-if="imgName || imgUrl"
-            class="gameImg"
-            :src="
-              imgName ? require('@/assets/gamecard/' + imgName + '') : imgUrl
-            "
-          />
+          <img v-if="imgName || imgUrl" class="gameImg" :src="
+            imgName ? require('@/assets/gamecard/' + imgName + '') : imgUrl
+          " />
         </div>
       </template>
       <template #title> {{ name }} </template>
       <template #subtitle>
-        <span :class="uptime.online ? 'online' : 'offline'">
+        <i v-if="uptime.friendlyName == ''" class="pi pi-spin pi-spinner" style="font-size: 1rem"></i>
+        <span v-else :class="uptime.online ? 'online' : 'offline'">
           {{ uptime.online ? "Online" : "Offline" }}
         </span>
       </template>
@@ -24,20 +21,9 @@
         </p>
       </template>
       <template #footer>
-        <Button
-          v-if="ip"
-          icon="pi pi-copy"
-          :label="'IP: ' + ip"
-          class="p-button bottom-0"
-          @click.prevent="doCopy"
-        />
-        <Button
-          v-if="joinUrl"
-          icon="pi pi-server"
-          :label="'Join now'"
-          class="p-button bottom-0"
-          @click.prevent="doJoin"
-        />
+        <Button v-if="ip" icon="pi pi-copy" :label="'IP: ' + ip" class="p-button bottom-0" @click.prevent="doCopy" />
+        <Button v-if="joinUrl" icon="pi pi-server" :label="'Join now'" class="p-button bottom-0"
+          @click.prevent="doJoin" />
       </template>
     </card>
   </div>
@@ -110,6 +96,7 @@ p {
   line-height: 1.5;
   margin: 0;
 }
+
 .gameImg {
   padding: 1rem 1rem 0;
   display: block;
@@ -118,26 +105,33 @@ p {
   width: auto;
   height: auto;
 }
+
 .online {
   color: green;
 }
+
 .offline {
   color: red;
 }
+
 .game-card:deep(.p-card-body) {
   flex-grow: 1;
   display: flex !important;
   flex-direction: column;
 }
+
 .game-card:deep(.p-card-content) {
   flex-grow: 1;
 }
+
 .game-card:deep(.p-card-footer) {
   padding: 0.5rem 0 0 0;
 }
+
 .game-card:deep(.p-button) {
   margin: 0.5rem 0.25rem 0;
 }
+
 .game-card:deep(.p-button:focus) {
   box-shadow: none !important;
 }
